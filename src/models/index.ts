0f295edca28170f5b7a42c862dbb07eb6ebb5d99
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-03-20 23:07:00 +0000

    fix: correct order of operations for first run

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,4 @@
-export {default as Game} from "./game.model";
 export {default as User} from "./user.model";
+export {default as Game} from "./game.model";
 export {default as Review} from "./review.model";
 export {default as Status} from "./status.model";
