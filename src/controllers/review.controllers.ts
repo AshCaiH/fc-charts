65279529ca7bd03c675406435a7728c30fea13eb
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-03-22 14:31:50 +0000

    feature: collect all reviews for a game

--- src/controllers/review.controllers.ts
+++ src/controllers/review.controllers.ts
@@ -1,37 +1,47 @@
 import { RequestHandler } from "express";
 import { sendError, sendMessage } from "../functions/responses";
 import { readFile } from 'fs/promises';
-import { filterReviews } from "../functions/reviews";
+import { fetchReviews, filterReviews } from "../functions/reviews";
+import { delay } from "../functions/common";
+import { Game, Review } from "../models";
 
 export const getReviews: RequestHandler = async (req, res) => {
     try {
         const game = req.game!;
         if (!game.ocId) throw Error(`${game.name} does not have a recorded OpenCritic ID.`)
 
-        const url = `https://opencritic-api.p.rapidapi.com/reviews/game/${game.ocId}?skip=${game.skipReviews}`;
-        const headers : HeadersInit = {
-            'X-RapidAPI-Key': process.env.OC_APIKEY!,
-            'X-RapidAPI-Host': 'opencritic-api.p.rapidapi.com'
-        }
+        const reviews = [];
+        let reviewCount = 0;
 
-        const options : RequestInit = {
-            method: 'GET',
-            headers: headers,
-        };
+        do {
+            const response = await fetchReviews(game).then(
+                async (response) => {
+                    const data = await response.json()
+                    reviewCount = data.length
+                    return filterReviews(data)
+                }
+            )
 
-        try {
-            const response = await fetch(url, options);
-            const result = await response.text();
-            console.log(result);
+            console.log("new reviews: " + reviewCount);
+            reviews.push(...response);
 
-            sendMessage(res, "Success", {response: response}, 201);
-        } catch (error) {
-            console.error(error);
-        }
+            response.map((review) => {
+                Review.create({
+                    ocId: review.ocId,
+                    ocScore: review.ocScore,
+                    date: review.date,
+                    GameId: game.id,
+                })
+            })
 
-        // await delay(250);
+            game.skipReviews += reviewCount;
+            
+            if (reviewCount == 20) await delay(250);
+        } while (reviewCount == 20);
+
+        await Game.update({skipReviews: game.skipReviews}, {where: {id: game.id}});
         
-        // sendMessage(res, "Success", {}, 201);
+        sendMessage(res, "Success", {reviews: reviews}, 201);
     } catch (error:any) {
         sendError(req, res, error);
     }
@@ -43,7 +53,6 @@ export const parseTestData: RequestHandler = async (req, res) => {
         const testreviews = await readFile("testfiles/yak8reviews.json", "utf8");
 
         const parsed = filterReviews(JSON.parse(testreviews));
-        console.log(parsed);
         
         sendMessage(res, "Success", {result: parsed}, 201);
     } catch (error:any) {
--- src/functions/reviews.ts
+++ src/functions/reviews.ts
@@ -1,15 +1,13 @@
-export const filterReviews = (reviews: any[]) => {
+import { Game } from "../models";
 
-    console.log(reviews);
+export const filterReviews = (reviews: any[]) => {
     
-    reviews = reviews.filter((review) => {
+    reviews = reviews.filter((review: any) => {
         if (review.Outlet.isContributor) return false;
         if (!review.ScoreFormat.isNumeric) return false;
         if (!review.score) return false;
         return true;
     });
-
-    console.log(reviews);
 
     return reviews.map(function(review) {
         return {
@@ -20,3 +18,22 @@ export const filterReviews = (reviews: any[]) => {
     });
 
 };
+
+export const fetchReviews = async (game: Game) : Promise<Response> => {
+
+    const url = `https://opencritic-api.p.rapidapi.com/reviews/game/${game.ocId}?skip=${game.skipReviews}`;
+    const headers : HeadersInit = {
+        'X-RapidAPI-Key': process.env.OC_APIKEY!,
+        'X-RapidAPI-Host': 'opencritic-api.p.rapidapi.com'
+    }
+
+    const options : RequestInit = {
+        method: 'GET',
+        headers: headers,
+    };    
+
+    const response = await fetch(url, options);
+    console.log("remaining requests: " + response.headers.get("x-ratelimit-requests-remaining"));
+    
+    return response;
+}
