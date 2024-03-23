968cf12d268fb585f8c454af244caecdd967da8e
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-03-23 14:31:17 +0000

    feature: record ratelimit status

--- src/routes/status.routes.ts
+++ src/routes/status.routes.ts
@@ -1,5 +1,13 @@
 import { Router } from "express";
+import { Status } from "../models";
+import { sendMessage } from "../functions/responses";
 
 const statusRouter: Router = Router();
+
+statusRouter.get("/status", (req, res, next) => {
+    Status.findAll({}).then(
+        (response) => sendMessage(res, "Success", {response}, 201)
+    )
+});
 
 export default statusRouter;
