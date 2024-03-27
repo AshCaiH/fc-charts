8067e7a31a9dd62a8dd847d90ea670445f95f160
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-03-27 10:59:11 +0000

    enhance: get chart data for all users

--- src/functions/charts.ts
+++ src/functions/charts.ts
@@ -1,43 +1,57 @@
 import { RequestHandler } from "express";
 import { sendError, sendMessage } from "./responses";
-import { Game, Review, User } from "../models";
+import { Game, Review, User, UserGames } from "../models";
 import { Op } from "sequelize";
 import { convertScore } from "./fantasycritic";
 
 export const generateChartData: RequestHandler = async (req, res) => {
     try {
-        const userGames = await Game.findAll({        
-            where: {ocScore: {[Op.ne]: null}},    
-            include: {
-                model: User,
-                where: {name: process.env.TEST_USER}
-            }
-        });
+        const users = await User.findAll({});
+        const result: any[] = [];
 
-        const userReviews: any[] = [];
-
-        for (const game of userGames) {
-            console.log(game.id);
-            const gameReviews = await Review.findAll({
-                where: {GameId: game.id}
-            }).then(item => {
-                let runningTotal = 0;
-
-                const parsedData = item.map((item, index) => {
-                    runningTotal += item.ocScore;
-                    const ocAverage = runningTotal / (index + 1);
-                    const fcAverage = convertScore(ocAverage);
-
-                    return {ocAverage: ocAverage, fcAverage: fcAverage, date: item.date};
-                });
-
-                return {game: game.name, data: parsedData}
+        for (const user of users) {
+            const userGames = await Game.findAll({        
+                where: {ocScore: {[Op.ne]: null}},    
+                include: {
+                    model: User,
+                    where: {id: user.id}
+                }
             });
 
-            userReviews.push(gameReviews);
+            const userReviews: any[] = [];
+
+            for (const game of userGames) {
+                console.log(game.id);
+                const gameReviews = await Review.findAll({
+                    where: {GameId: game.id}
+                }).then(item => {
+                    let runningTotal = 0;
+
+                    const counterpicked = UserGames.findOne({
+                        where: {
+                            UserId: user.id,
+                            GameId: game.id,
+                        }
+                    }).then(response => response!.counterpicked);
+
+                    const parsedData = item.map((item, index) => {
+                        runningTotal += item.ocScore;
+                        const ocAverage = runningTotal / (index + 1);
+                        const fcAverage = convertScore(ocAverage);
+
+                        return {ocAverage: ocAverage, fcAverage: fcAverage, date: item.date};
+                    });
+
+                    return {game: game.name, counterpicked: counterpicked, data: parsedData}
+                });
+
+                userReviews.push(gameReviews);
+            }
+
+            result.push({user: user.name, reviews: userReviews});
         }
         
-        sendMessage(res, "Retrieved reviews for user", {result: userReviews}, 201);
+        sendMessage(res, "Retrieved reviews for user", {result: result}, 201);
     } catch (error:any) {
         sendError(req, res, error);
     }
