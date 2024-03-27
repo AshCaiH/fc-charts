5492750bf5b54b9236c6ee16d203e8f6d195b4a1
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-03-27 16:34:23 +0000

    chore: allow server user to set sql logs on or off from env file

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,6 +1,7 @@
 import { Sequelize } from "sequelize-typescript";
 
 const sequelize = new Sequelize(process.env.MYSQL_URI || "");
+sequelize.options.logging = Boolean(process.env.SQL_LOGS) || true;
 sequelize.authenticate();
 
 console.log("DB Connection is working");
