8b0d5c95f4fe5483e723ace91bd50d82adc71158
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-05-30 13:39:27 +0100

    chore: set up cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import "dotenv/config.js";
 import express, { Request, Response, json } from "express";
 import { User, Game, UserGames, Review } from "./models";
+import cors from "cors";
 import * as models from "./models";
 import * as routers from "./routes";
 import * as types from "./custom"; // Required for custom Request properties.
@@ -9,7 +10,7 @@ const port = process.env.PORT || 5001;
 
 const app = express();
 
-app.use(json(), ...Object.values(routers));
+app.use(json(), cors(), ...Object.values(routers));
 
 app.get("/health", (req:Request, res:Response) => {
     res.status(200).json({message: "API is healthy"});
